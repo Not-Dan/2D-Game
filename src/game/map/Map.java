f4b01a58babb16db5651492648f1a09f2bdcbe98
24Caleb Rickenbach <24CRickenbach@htsp.Local>
2023-03-01 16:34:20 -0700

    Classes created, feel free to change if limiting

---
+++ src/game/map/Map.java
@@ -0,0 +1,16 @@
+package game.map;
+
+//Created by Jahrr on 3/1/23
+
+
+//This will hold our map objects. We can create many of these classes to account for different levels
+//or worlds. Note that this does not contain maps, we need to create a maploader or mapmanager class,
+//as well as a world class to contain and load these objects. These should not be accessed directly,
+//rather loaded in with our world class and switched to through the world class interface.
+public class Map {
+
+
+
+
+
+}
---
+++ src/game/map/MapCreator.java
@@ -0,0 +1,13 @@
+package game.map;
+
+//Created by Jahrr on 3/1/23
+
+//Perhaps we should create a new debug package and store this class there
+
+
+//This is our tilemap editor contained in a class. It should contain a accessible scene
+//that can be run in debug to edit our maps. We should be able to load multiple tilemaps,
+//but to start focus on loading one tilemap for all of our maps.
+public class MapCreator
+{
+}
